dbf0173d8741e0b536f1d3dac4efd434cb801fb6
Travis Russell <travis.russell@maine.edu>
2022-11-12 21:53:35 -0500

    initial commit

---
+++ .vimrc
@@ -0,0 +1,11 @@
+if has('termguicolors')
+	set termguicolors
+endif
+
+syntax on
+set nu
+set rnu
+set background=dark
+let g:everforest_background = 'dark'
+let g:everforest_better_performance = 1
+colo everforest
---
+++ everforest.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# this script must be run from within the directory the script and the template .vimrc file exists within
+
+git clone https://github.com/sainnhe/everforest.git
+
+mkdir -p ~/.vim/{autoload,colors,doc}
+
+cp everforest/autoload/everforest.vim ~/.vim/autoload/
+
+cp everforest/colors/everforest.vim ~/.vim/colors/
+
+cp everforest/doc/everforest.txt ~/.vim/doc/
+
+cp .vimrc ~/.vimrc
+
+
+
